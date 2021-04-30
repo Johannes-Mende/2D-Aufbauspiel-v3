d488b27618c91b3ba47bcda2873e120aba11364d
Johannes Mende <jomende99@gmx.de>
2021-04-30 21:32:53 +0200

    Szenemanager.cs Angelegt
    mit region Aufgabe

---
+++ Assets/Scripts/Szenemanager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Szenemanager : MonoBehaviour
+{
+    #region Aufgabe
+    /*
+        Der Szenemanager soll alles Regeln was mit Szenen zu tun hat.
+        
+        Spacestation
+        Also wenn zum beispiel der Spieler eine neue Space Station baut und auf den bauen button klickt kann er erstmal auswählen wo sie hin soll und dann sagt er wie sie heißen soll
+        mit dem Bestetigen wird die neue Szene mit dem Namen der Station und einem Basic Spacestation tile erstellt.
+
+        World Generation
+        wenn der Spieler auf einen Neuen Planeten kommt wird eine Neue Szene angelegt mit der dazugehörigen Tilemap (keine Ahnung wie das mit Biomen ist)
+
+    */
+    #endregion
+
+    
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
