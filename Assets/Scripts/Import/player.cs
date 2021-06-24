2b81df5712e6f59fb6d92446b2e3d932559d3193
Johannes Mende <jomende99@gmx.de>
2021-06-24 16:03:32 +0200

    Tiles Abbauen hinzugefügt
    Upgrade auf 2020.3.5f1

--- Assets/Scripts/Import/player.cs
+++ Assets/Scripts/Import/player.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Tilemaps;
 public class player : MonoBehaviour
 {
     
@@ -10,16 +10,36 @@ public class player : MonoBehaviour
     public LayerMask layer;
     public float rayDistance;
     public Vector2 jumpForce;
+
+
+    public GameObject selectedObject;       //Raycasthit2D
+    public Tilemap tiles;
+    public Tile tile;
+    public Vector3Int location;
+
     void Awake()
     {
         Application.targetFrameRate = 60;
         access = this;
+        tiles = GameObject.Find("Tilemap").GetComponent<Tilemap>(); //GetT
     }
     
     private void Update()
     {
         grounded = IsGrounded();
         Debug.DrawRay (transform.position, -Vector2.up, Color.red, rayDistance);
+        Mining();
+
+        if(Input.GetMouseButton(0))
+        {
+            Vector3 mp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            location = tiles.WorldToCell(mp);
+            tiles.SetTile(location, tile);
+        }
+        if(Input.GetMouseButtonDown(1))
+        {
+            GetT();
+        }
     }
     
     bool IsGrounded()
@@ -40,4 +60,35 @@ public class player : MonoBehaviour
         
     }
     
+    public void Mining()
+    {
+        /*Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hitData = Physics2D.Raycast(new Vector2(worldPosition.x, worldPosition.y), Vector2.zero, 0, LayerMask.GetMask("Tilemap"));
+
+    
+           if (hitData && Input.GetMouseButtonDown(0))
+                {
+                    Debug.Log("Hit und Weg");                       //https://www.youtube.com/watch?v=zABMvbTyEK8
+                    selectedObject = hitData.transform.gameObject;
+                    worldPosition = tiles.WorldToCell();
+                    Destroy(selectedObject);
+                }
+        */
+
+    }
+
+    void GetT()
+    {
+        Vector3 mp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        tiles.SetTile(location, tile);
+
+        if(tiles.GetTile(location))
+        {
+            Debug.Log("Tile");
+        }
+        else
+        {
+            Debug.Log("No Tile");
+        }
+    }
 }
